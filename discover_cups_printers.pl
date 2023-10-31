fbe787f8842677c588ebe92f9e50a77a36a32543
paulopaag <paulopaag@users.noreply.github.com>
2023-10-31 09:45:06 -0300

    Add files via upload

---
+++ discover_cups_printers.pl
@@ -0,0 +1,18 @@
+#!/bin/bash
+export LANG=C;
+printers=$(lpstat -p | grep "^printer" | cut -f2 -d ' ')
+# printers=$(($printers-1))
+isfirst=1
+echo "{"
+echo "\"data\":["
+for printer in $printers
+do
+    if [ "$isfirst" == "1" ] ; then
+	isfirst=0
+    else
+	echo ","
+    fi
+    echo "    {\"{#PRINTER}\":\"$printer\"}"
+done
+echo "]"
+echo "}"
